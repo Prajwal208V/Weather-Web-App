e111c206d696686e9e2821b76db3921a6a23855a
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2022-04-27 23:50:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ var hum=document.querySelector('.paragraph2');
 var wind=document.querySelector('.paragraph3');
 var cloud=document.querySelector('.paragraph4');
 function fun(para1){
-    var wether = new XMLHttpRequest();
-    wether.open('GET', `http://api.openweathermap.org/data/2.5/weather?q=${para1}&appid=d3b6aaa7c8ae98a5395f93b296e793d1`);
-    wether.send();
-    wether.addEventListener('load', function () {
-        const data = JSON.parse(this.responseText);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${para1}&appid=d3b6aaa7c8ae98a5395f93b296e793d1`)
+    .then(res=> {
+        if(!res.ok){
+            throw res.json();
+        }
+        return res.json()
+    })
+    .then(data=>{
         console.log(data);
         city_name.textContent=data.name;
         celi.textContent=Math.round(((data.main.temp)-273.15) * 10) / 10;
@@ -19,14 +22,19 @@ function fun(para1){
         hum.textContent=`Humidity: ${data.main.humidity}%`;
         wind.textContent=`Wind-Speed: ${Math.round((data.wind.speed)*18/5*10)/10} km/h`;
         cloud.textContent=`clouds: ${data.clouds.all} %`
-    });
+    })
+    .catch(err=>{
+        err.then((data)=>{
+            alert(data.message);
+        })
+    })
+    
 }
 (()=>{
     fun('Bengaluru');
 })();
 inp.addEventListener('keyup', function(e) {
     if(e.keyCode===13){
-      console.log(inp.value);
       fun(inp.value);
     }
 });
